7b8ad5668859105591523ccca56d31e2a1cbf5ff
Fujio Kojima <fujiwo@hotmail.co.jp>
2023-10-24 17:48:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Shos.DI.Sample/Program.cs
@@ -0,0 +1,38 @@
+﻿using Shos.DI;
+
+var container = new DIContainer();
+
+container.Register<Boo1>();
+container.Register<Boo2>();
+container.Register<Boo3>();
+container.Register<Foo>();
+
+var foo = container.GetInstance<Foo>();
+Console.WriteLine(foo ?? "Error");
+
+class Coo
+{
+    public Coo() => Console.WriteLine(nameof(Coo));
+}
+
+class Boo1
+{
+    public Boo1() => Console.WriteLine(nameof(Boo1));
+}
+
+class Boo2
+{
+    public Boo2(Coo coo) => Console.WriteLine($"{nameof(Boo2)}({nameof(Coo)})");
+}
+
+class Boo3
+{
+    public Boo3(Boo1 boo1) => Console.WriteLine($"{nameof(Boo3)}({nameof(Boo1)})");
+}
+
+class Foo
+{
+    public Foo(Boo2 boo2, Boo3 boo3) => Console.WriteLine($"{nameof(Foo)}({nameof(Boo2)}, {nameof(Boo3)})");
+
+    public override string ToString() => $"{nameof(Foo)}";
+}
---
+++ Shos.DI/DIContainer.cs
@@ -0,0 +1,149 @@
+﻿using System.Reflection;
+
+namespace Shos.DI
+{
+    static class Extensions
+    {
+        public static void ForEach<TElement>(this IEnumerable<TElement> @this, Action<TElement> action)
+        {
+            foreach (var element in @this)
+                action(element);
+        }
+
+        public static object? GetInstance(this Type @this)
+            => @this.GetConstructor(new Type[0])?.Invoke(new object[0]);
+    }
+
+    public class DIContainer
+    {
+        Dictionary<Type, TypeInformation> typeInformations = new();
+
+        public object? GetInstance<T>() => GetInstance(typeof(T));
+
+        object? GetInstance(Type type)
+        {
+            var instance = type.GetInstance();
+            if (instance is not null)
+                return instance;
+
+            if (typeInformations.TryGetValue(type, out var typeInformation)) {
+                instance = typeInformation.GetInstance();
+                if (instance is not null)
+                    return instance;
+
+                foreach (var constructor in typeInformation.Constructors) {
+                    var parameterTypes = typeInformation[constructor];
+                    if (parameterTypes is null)
+                        continue;
+                    var parameters = parameterTypes.Select(parameterType => GetInstance(parameterType)).ToArray();
+                    if (parameters.Any(parameter => parameter is null))
+                        continue;
+                    return constructor.Invoke(parameters);
+                }
+            }
+            return null;
+        }
+
+        public void Register<T>() => Register(typeof(T));
+        void Register(Type type) => typeInformations[type] = new TypeInformation(type);
+    }
+
+    public class TypeInformation
+    {
+        Dictionary<Type[], ConstructorInfo>? constructorInfoTable = null;
+        Dictionary<ConstructorInfo, Type[]>? parameterTypesTable  = null;
+        Dictionary<Type[], object         >? instanceTable        = null;
+        ConstructorInfo[]?                   constructors         = null;
+
+        public Type Type { get; private set; }
+
+        public IEnumerable<ConstructorInfo> Constructors {
+            get {
+                Initialize();
+                return constructors!;
+            }
+        }
+
+        public Type[]? this[ConstructorInfo constructor] {
+            private set {
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value));
+                if (parameterTypesTable is null)
+                    parameterTypesTable = new();
+                parameterTypesTable[constructor] = value;
+            }
+            get {
+                Initialize();
+                if (parameterTypesTable is null)
+                    return null;
+                return  parameterTypesTable.TryGetValue(constructor, out var parameterTypes) ? parameterTypes : null;
+            }
+        }
+
+        ConstructorInfo? this[Type[] parameter] {
+            set {
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value));
+                if (constructorInfoTable is null)
+                    constructorInfoTable = new();
+                constructorInfoTable[parameter] = value;
+            }
+            get {
+                Initialize();
+                if (constructorInfoTable is null)
+                    return null;
+                return constructorInfoTable.TryGetValue(parameter, out var constructor) ? constructor : null;
+            }
+        }
+
+        public TypeInformation(Type type) => Type = type;
+
+        public object? GetInstance() => GetInstance(new object[0]);
+
+        public object? GetInstance(object[] parameters)
+        {
+            Initialize();
+
+            var parameterTypes = parameters.Select(parameter => parameter.GetType()).ToArray();
+            var instance       = GetInstance(parameterTypes);
+            if (instance is not null)
+                return instance;
+
+            if (constructorInfoTable is null || !constructorInfoTable.TryGetValue(parameterTypes, out var constructor))
+                return null;
+
+            instance           = constructor.Invoke(parameters);
+            SetInstance(parameterTypes, instance);
+            return instance;
+        }
+
+        void Initialize()
+        {
+            if (constructors is not null)
+                return;
+
+            constructors = Type.GetConstructors();
+            var pairs    = constructors.Select(constructor => (constructor.GetParameters().Select(parameterInfo => parameterInfo.ParameterType).ToArray(), constructor)).ToList();
+
+            pairs.ForEach(pair => {
+                this[pair.Item1] = pair.Item2;
+                this[pair.Item2] = pair.Item1;
+            });
+        }
+
+        void SetInstance(Type[] parameterTypes, object instance)
+        {
+            if (instanceTable is null)
+                instanceTable = new();
+            instanceTable[parameterTypes] = instance;
+        }
+
+        object? GetInstance(Type[] parameterTypes)
+        {
+            Initialize();
+            if (instanceTable is null)
+                return null;
+            return instanceTable.TryGetValue(parameterTypes, out var instance) ? instance : null;
+        }
+    }
+}
